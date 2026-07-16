0cb4fec8cbef40c6efb64641d57d560589576a06
variannn340 <varianvarian0301@gmail.com>
2026-07-16 13:45:31 +0700

    done

--- Elself.cpp
+++ Elself.cpp
@@ -12,10 +12,23 @@ else
     return "Gagal";
 }
 
-
-
-
-
 int main(){
+    float nilBI,nilMT,rerata;
+    string status;
+
+    cout <<"Masukkan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout <<"Masukkan nilai Matematika = ";
+    cin >> nilMT;
+
+    //rerata = (nilBI+nilMT)/2;
+    //if (rerata >= 60)
+    //    status = "Lolos";
+    //else
+      // status = "Gagal";
+    rerata = Rata_rata(nilBI,nilMT);
+    //status = Status_lulus(rerata);
+
+    cout << "Status kelulusan = "<<Status_lulus(rerata)<<" ,dengan nilai rerata = "<<rerata<<endl;
 
 }
