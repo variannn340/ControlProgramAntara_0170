02355911f93f4333548a5a47d451af105080be4d
variannn340 <varianvarian0301@gmail.com>
2026-07-16 13:55:49 +0700

    done

--- NestedIf.cpp
+++ NestedIf.cpp
@@ -18,5 +18,22 @@ string Status_lulus(float r){
 
 
 int main(){
-    
+    float nilBI,nilMT,rerata;
+    string status;
+
+    cout <<"Masukkan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout <<"Masukkan nilai Matematika = ";
+    cin >> nilMT;
+
+    //rerata = (nilBI+nilMT)/2;
+    //if (rerata >= 60)
+    //    status = "Lolos";
+    //else
+      // status = "Gagal";
+    rerata = Rata_rata(nilBI,nilMT);
+    //status = Status_lulus(rerata);
+
+    cout << "Status kelulusan = "<<Status_lulus(rerata)<<" ,dengan nilai rerata = "<<rerata<<endl;
+
 }
