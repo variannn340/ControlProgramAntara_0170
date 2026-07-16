2e72950357278dcb295ad8ceda15f336f63ae5bf
variannn340 <varianvarian0301@gmail.com>
2026-07-16 14:00:36 +0700

    done

--- SimpleIf.cpp
+++ SimpleIf.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main(){
-    
+    int x;
+
+    srand(time(0));
+    x = rand() % 10;
+
+    if (x==5){
+        x = x+1;
+        cout<<"nilai x sudah dikoreksi menjadi : "<<x<<endl;
+    }
+
+    cout << "Bilangan yang diberikan = "<<x;
 }
